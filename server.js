36018cd94807ef3fd4d7cc61ee7b7a5c71915243
shuchitamathur <shuchita.mathur@mayuraconsultancy.com>
2025-06-01 15:26:54 +0530

    changed cors setting to allow request from vercel frontend

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors({
   // origin: 'http://localhost:5173', 
-  origin: 'https://finance-tracker-frontend.onrender.com', // Update
+  origin: 'https://finance-tracker-frontend-five.vercel.app', // Update
   credentials: true               
 }));
 
